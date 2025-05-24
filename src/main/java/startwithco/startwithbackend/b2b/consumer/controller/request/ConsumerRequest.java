a79a9776bc0868be7b1b8e2f2937ad56ba466c76
lynjun <lynjun97@naver.com>
2025-05-24 17:31:28 +0900

    consumer 가입 구현

---
+++ src/main/java/startwithco/startwithbackend/b2b/consumer/controller/request/ConsumerRequest.java
@@ -0,0 +1,42 @@
+package startwithco.startwithbackend.b2b.consumer.controller.request;
+
+import startwithco.startwithbackend.exception.badRequest.BadRequestErrorResult;
+import startwithco.startwithbackend.exception.badRequest.BadRequestException;
+
+public class ConsumerRequest {
+
+    public record SaveVendorRequest(
+            String vendorName,
+            String password,
+            String phoneNum,
+            String email
+    ) {
+    }
+
+    public record SaveConsumerRequest(
+            String consumerName,
+            String password,
+            String email,
+            String industry
+    ) {
+
+        public void validateSaveConsumerRequest(SaveConsumerRequest request){
+
+            if (request.consumerName() == null || request.email() == null
+                    || request.password() == null || request.industry() == null) {
+                throw new BadRequestException(BadRequestErrorResult.BAD_REQUEST_EXCEPTION);
+            }
+
+        }
+    }
+
+    public record LoginRequest(
+            String email,
+            String password
+    ) {
+    }
+
+
+
+}
+
