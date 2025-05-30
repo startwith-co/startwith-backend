ed977d2e53dad253c97326ab1b4d08927f997dc0
lynjun <lynjun97@naver.com>
2025-05-30 22:52:17 +0900

    consumer 가입 수정

--- src/main/java/startwithco/startwithbackend/b2b/consumer/domain/ConsumerEntity.java
+++ src/main/java/startwithco/startwithbackend/b2b/consumer/domain/ConsumerEntity.java
@@ -29,6 +29,9 @@ public class ConsumerEntity extends BaseTimeEntity {
     @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
+    @Column(name="phone_num", nullable = false)
+    private String phoneNum;
+
     @Column(name = "encoded_password", nullable = false)
     private String encodedPassword;
 
