67f5713949e7afd94548d26f335b5efad3dada32
lynjun <lynjun97@naver.com>
2025-06-01 21:58:58 +0900

    consumer 수정

--- src/main/java/startwithco/startwithbackend/b2b/consumer/controller/request/ConsumerRequest.java
+++ src/main/java/startwithco/startwithbackend/b2b/consumer/controller/request/ConsumerRequest.java
@@ -1,5 +1,6 @@
 package startwithco.startwithbackend.b2b.consumer.controller.request;
 
+import jakarta.persistence.Column;
 import org.springframework.http.HttpStatus;
 import startwithco.startwithbackend.b2b.vendor.controller.request.VendorRequest;
 import startwithco.startwithbackend.exception.BadRequestException;
@@ -17,7 +18,7 @@ public class ConsumerRequest {
             String industry
     ) {
 
-        public void validateSaveConsumerRequest(SaveConsumerRequest request){
+        public void validateSaveConsumerRequest(SaveConsumerRequest request) {
 
             if (request.consumerName() == null || request.email() == null || request.phoneNum() == null
                     || request.password() == null || request.confirmPassword() == null || request.industry() == null) {
@@ -28,7 +29,7 @@ public class ConsumerRequest {
                 );
             }
 
-            if(!request.password().equals(request.confirmPassword())) {
+            if (!request.password().equals(request.confirmPassword())) {
                 throw new BadRequestException(
                         HttpStatus.BAD_REQUEST.value(),
                         "요청 데이터 오류입니다.",
@@ -39,13 +40,33 @@ public class ConsumerRequest {
         }
     }
 
-    public record LoginConsumerRequest (
+    public record LoginConsumerRequest(
             String email,
             String password
     ) {
         public void validateLoginConsumerRequest(LoginConsumerRequest request) {
 
             if (request.email == null || request.password == null) {
+                throw new BadRequestException(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "요청 데이터 오류입니다.",
+                        getCode("요청 데이터 오류입니다.", ExceptionCodeMapper.ExceptionType.BAD_REQUEST)
+                );
+            }
+        }
+    }
+
+    public record UpdateConsumerInfoRequest(
+            Long consumerSeq,
+            String consumerName,
+            String phoneNumber,
+            String encodedPassword,
+            String email,
+            String industry
+    ) {
+
+        public void validateUpdateConsumerRequest(UpdateConsumerInfoRequest request) {
+            if (request.consumerSeq == null) {
                 throw new BadRequestException(
                         HttpStatus.BAD_REQUEST.value(),
                         "요청 데이터 오류입니다.",
