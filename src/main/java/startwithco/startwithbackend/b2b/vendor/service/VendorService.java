ed23314d18e9ebb7bfc9c2532f22e7d30467aecb
lynjun <lynjun97@naver.com>
2025-09-09 20:23:26 +0900

    vendor profile Image update 추가

--- src/main/java/startwithco/startwithbackend/b2b/vendor/service/VendorService.java
+++ src/main/java/startwithco/startwithbackend/b2b/vendor/service/VendorService.java
@@ -233,7 +233,7 @@ public class VendorService {
     }
 
     @Transactional
-    public void updateVendor(UpdateVendorInfoRequest request, MultipartFile vendorBannerImageUrl, List<MultipartFile> clientInfos) {
+    public void updateVendor(UpdateVendorInfoRequest request, MultipartFile vendorBannerImageUrl, List<MultipartFile> clientInfos, MultipartFile profileImage) {
         VendorEntity vendorEntity = vendorEntityRepository.findByVendorSeq(request.vendorSeq())
                 .orElseThrow(() -> new NotFoundException(
                         HttpStatus.NOT_FOUND.value(),
@@ -241,7 +241,8 @@ public class VendorService {
                         getCode("존재하지 않는 벤더 기업입니다.", ExceptionType.NOT_FOUND)
                 ));
 
-        String uploadJPGFile = vendorBannerImageUrl == null ? null : commonService.uploadJPGFile(vendorBannerImageUrl);
+        String vendorBannerImage = vendorBannerImageUrl == null ? null : commonService.uploadJPGFile(vendorBannerImageUrl);
+        String vendorProfileImage = vendorBannerImageUrl == null ? null : commonService.uploadJPGFile(vendorBannerImageUrl);
 
         vendorEntity.update(
                 request.vendorName(),
@@ -252,7 +253,7 @@ public class VendorService {
                 request.accountNumber(),
                 request.bank(),
                 request.vendorExplanation(),
-                uploadJPGFile,
+                vendorBannerImage,
                 request.weekdayAvailable(),
                 request.weekdayStartTime(),
                 request.weekdayEndTime(),
@@ -263,7 +264,8 @@ public class VendorService {
                 request.holidayStartTime(),
                 request.holidayEndTime(),
                 request.orderCount(),
-                request.clientCount()
+                request.clientCount(),
+                vendorProfileImage
         );
 
         vendorEntityRepository.save(vendorEntity);
